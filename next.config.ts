26fdee929ee9ce71804342509cb79e34eaa4baeb
bug1k <sergey.dev.987@gmail.com>
2025-07-27 23:40:54 +0800

    0000000

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverActions: true,
     css: {
-      lightningcss: false,
-    },
-  },
-};
+      lightningcss: false
+    }
+  }
+}
 
 module.exports = nextConfig;
